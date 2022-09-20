4744741e2ce0022d7f4336158c654272da805f51
ezn6 <kcourtney@naver.com>
2022-09-20 20:00:57 +0900

    init project

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Grid from '@mui/material/Unstable_Grid2';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { Container, Item } from './Main.style';
+
+// mui tutorial
+function Main() {
+  return (
+    <Container maxWidth={"1000px"} sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid xs={8}>
+          <Item>xs=8</Item>
+        </Grid>
+        <Grid xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid xs={8}>
+          <Item>xs=8</Item>
+        </Grid>
+      </Grid>
+      <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+    </Container>
+  );
+}
+
+export default Main;
---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+
+import { red } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+  },
+});
+export default theme
