b81dd598a344b503afbfce894e38a21041b9b4ca
Malza0408 <parkjh04080@gmail.com>
2022-09-21 17:23:35 +0900

    Feat: 추가 User page Component에 받아온 userList 렌더링

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,8 +1,11 @@
+import React, { useEffect } from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsersList } from '../../features/users/usersSlice';
+import { API_STATUS } from '../../common/utils/constant';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -13,11 +16,28 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 function User() {
-  return (
-    <Box minWidth={'1200px'}>
+  const dispatch = useDispatch();
+  const { users, status } = useSelector(state => state.usersReducer);
+  console.info(users);
+  useEffect(() => {
+    const getUserList = async () => {
+      await dispatch(fetchUsersList()).unwrap();
+    };
+    getUserList();
+  }, []);
+
+  return status === API_STATUS.LOADING ? (
+    <div>Loading...</div>
+  ) : (
+    <Box minWidth={'1500px'}>
       <Grid container>
         <Grid item xs={1}>
           <Item>고객명</Item>
+          <ul>
+            {users.map(user => (
+              <li key={user.uuid}>{user.name}</li>
+            ))}
+          </ul>
         </Grid>
         <Grid item xs={1}>
           <Item>계좌 수</Item>
