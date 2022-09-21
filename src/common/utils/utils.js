01fe6ed2d9522a668fd4a781b71583875b2e40f9
Malza0408 <parkjh04080@gmail.com>
2022-09-22 00:47:39 +0900

    Feat: 추가 maskingName 함수
    name 길이에 따라 masking 해 줄 util 함수 작성

--- src/common/utils/utils.js
+++ src/common/utils/utils.js
@@ -1,3 +1,32 @@
 export const convertDate = date => {
   return date.split('T')[0];
 };
+
+export const spaceName = splittedName => {
+  return ' ' + splittedName + ' ';
+};
+
+export const maskingName = name => {
+  const mask = '*';
+  const splittedName = name.split(' ');
+
+  if (splittedName.length === 2) {
+    splittedName[0] = mask.repeat(splittedName[0].length);
+    splittedName[0] = spaceName(splittedName[0]);
+    return splittedName;
+  }
+
+  if (splittedName.length === 3) {
+    splittedName[1] = mask.repeat(splittedName[1].length);
+    splittedName[1] = spaceName(splittedName[1]);
+    return splittedName;
+  }
+
+  if (splittedName.length === 4) {
+    splittedName[1] = mask.repeat(splittedName[1].length);
+    splittedName[1] = spaceName(splittedName[1]);
+    splittedName[2] = mask.repeat(splittedName[2].length);
+    splittedName[2] = spaceName(splittedName[2]);
+    return splittedName;
+  }
+};
