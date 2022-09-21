a69971ab51d4e70b70d596a679738730ddcde405
Malza0408 <parkjh04080@gmail.com>
2022-09-21 14:33:40 +0900

    Feat: 추가 root인 index파일에 redux Provider 적용

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import App from './App';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from './styles/theme';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
