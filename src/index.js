5176ca74ca46ee86607a193c878c8f94cabc25cb
ezn6 <kcourtney@naver.com>
2022-09-21 15:27:47 +0900

    Feat: 추가 userSlice
    user정보를 관리할 userSlice와 store생성

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import App from './App';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from './styles/theme';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './modules/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+  // <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
-    </ThemeProvider>
-  </React.StrictMode>
+  </ThemeProvider>
+  // </React.StrictMode>
 );
