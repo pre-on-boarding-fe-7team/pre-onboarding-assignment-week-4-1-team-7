756b581be4981fb9b997a5a23ceb2b009e1748f5
Malza0408 <parkjh04080@gmail.com>
2022-09-22 03:27:15 +0900

    Feat: 추가 phone number masking 함수
    phone number의 중간 번호 4개를 *로 표시하기 위해 변환하는 함수 작성

--- src/common/utils/utils.js
+++ src/common/utils/utils.js
@@ -10,15 +10,15 @@ export const maskingName = name => {
   const mask = '*';
   const splittedName = name.split(' ');
 
-  if (splittedName.length === 2) {
-    splittedName[0] = mask.repeat(splittedName[0].length);
-    splittedName[0] = spaceName(splittedName[0]);
-    return splittedName;
-  }
-
   if (splittedName.length === 3) {
     splittedName[1] = mask.repeat(splittedName[1].length);
     splittedName[1] = spaceName(splittedName[1]);
+    return splittedName;
+  }
+
+  if (splittedName.length === 2) {
+    splittedName[0] = mask.repeat(splittedName[0].length);
+    splittedName[0] = spaceName(splittedName[0]);
     return splittedName;
   }
 
@@ -29,4 +29,16 @@ export const maskingName = name => {
     splittedName[2] = spaceName(splittedName[2]);
     return splittedName;
   }
+};
+
+export const dashPhoneNumber = splittedNumber => {
+  return '-' + splittedNumber + '-';
+};
+
+export const maskingPhonNumber = phoneNumber => {
+  const mask = '*';
+  const splittedNumber = phoneNumber.split('-');
+  splittedNumber[1] = mask.repeat(splittedNumber[1].length);
+  splittedNumber[1] = dashPhoneNumber(splittedNumber[1]);
+  return splittedNumber;
 };
