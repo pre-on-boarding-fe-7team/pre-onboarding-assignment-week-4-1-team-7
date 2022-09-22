babc75d6e3d16d3f1b9b13d07079d270aadb2de2
cssong <a1373052011@gmail.com>
2022-09-22 20:53:30 +0900

    Feat: 변경, 계좌목록 로직 변경
    상위 컴포넌트에서 api 호출하던 부분을 하위 컴포넌트 AccountList 로 이동
    상위에서 state를 관리하면서 페이지 전체가 재렌더링되는 문제가 발생

--- src/pages/Accounts/AccountList/AccountList.jsx
+++ src/pages/Accounts/AccountList/AccountList.jsx
@@ -1,8 +1,13 @@
-import { Box, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import { Box, Card, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
 import useAxios from '../../../common/hooks/useAxios';
 import { boolToIcon, makeGetUserName } from '../../../common/utils/field.util';
 import { getUsersApi } from '../../../api/api';
 import Loading from '../../../components/Loading/Loading';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { fetchAccounts } from '../../../modules/accountsSlice';
+import AccountPagination from '../AccountPagination';
 
 const header = [
   'user_name',
@@ -16,40 +21,56 @@ const header = [
   'created_at',
 ];
 
-const AccountList = ({ accounts }) => {
-  const { data, loading, error } = useAxios(getUsersApi);
-  const getUserName = makeGetUserName(data);
+const AccountList = () => {
+  const dispatch = useDispatch();
+  const accounts = useSelector(state => state.accounts);
+  const users = useAxios(getUsersApi);
+  const getUserName = makeGetUserName(users.data);
+  const [searchParams] = useSearchParams();
+  const _page = Number.parseInt(searchParams.get('page') || 1);
+  const q = searchParams.get('q');
+  const broker_id = searchParams.get('broker');
+  const is_active = searchParams.get('active');
 
-  if (loading) return <Loading />;
-  if (error) return <p>에러</p>;
-  if (data)
+  useEffect(() => {
+    dispatch(fetchAccounts({ _page, q, broker_id, is_active }));
+  }, [dispatch, _page, q, broker_id, is_active]);
+
+  if (accounts.loading || users.loading) return <Loading />;
+  if (accounts.error || users.error) return <p>에러</p>;
+  if (accounts.data && users.data)
     return (
-      // <Box sx={{ minWidth: 1050, height: 400, overflowY: 'scroll' }}>
-      <Box sx={{ minWidth: 1050 }}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              {header.map((value, index) => (
-                <TableCell key={index}>{value}</TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {accounts.map(account => (
-              <TableRow key={account.uuid}>
-                <TableCell>{getUserName(account.id)}</TableCell>
-                <TableCell>{account.broker_id}</TableCell>
-                <TableCell>{account.number}</TableCell>
-                <TableCell>{account.status}</TableCell>
-                <TableCell>{account.name}</TableCell>
-                <TableCell>{account.assets}</TableCell>
-                <TableCell>{account.payments}</TableCell>
-                <TableCell>{boolToIcon(account.is_active)}</TableCell>
-                <TableCell>{account.created_at}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+      <Box sx={{ mt: 3 }}>
+        <Card>
+          {/* // <Box sx={{ minWidth: 1050, height: 400, overflowY: 'scroll' }}> */}
+          <Box sx={{ minWidth: 1050 }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  {header.map((value, index) => (
+                    <TableCell key={index}>{value}</TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {accounts.data.data.map(account => (
+                  <TableRow key={account.uuid}>
+                    <TableCell>{getUserName(account.id)}</TableCell>
+                    <TableCell>{account.broker_id}</TableCell>
+                    <TableCell>{account.number}</TableCell>
+                    <TableCell>{account.status}</TableCell>
+                    <TableCell>{account.name}</TableCell>
+                    <TableCell>{account.assets}</TableCell>
+                    <TableCell>{account.payments}</TableCell>
+                    <TableCell>{boolToIcon(account.is_active)}</TableCell>
+                    <TableCell>{account.created_at}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Box>
+          <AccountPagination total={accounts.data.total} />
+        </Card>
       </Box>
     );
 };
