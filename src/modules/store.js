23b88046464879f0b67c3d0e15aa33b1947f48f8
cssong <a1373052011@gmail.com>
2022-09-22 22:31:36 +0900

    Feat: 추가, 고객명 변환에 필요한 userSlice

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import accountsSlice from './accountsSlice';
+import userSlice from './userSlice';
 
 const store = configureStore({
-  reducer: { accounts: accountsSlice.reducer },
+  reducer: { accounts: accountsSlice.reducer, users: userSlice },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
---
+++ src/modules/userSlice.js
@@ -0,0 +1,29 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { getUsersApi } from '../api/api';
+
+export const getUsersFetch = createAsyncThunk('userSlice/getUsersFetch', async () => {
+  return await getUsersApi();
+});
+
+const userSlice = createSlice({
+  name: 'userSlice',
+  initialState: {
+    users: null,
+    user: null,
+    status: 'Hello',
+  },
+  extraReducers: builder => {
+    builder.addCase(getUsersFetch.pending, (state, action) => {
+      state.status = 'Loading';
+    });
+    builder.addCase(getUsersFetch.fulfilled, (state, action) => {
+      state.users = action.payload;
+      // state.status = 'complete';
+    });
+    builder.addCase(getUsersFetch.rejected, (state, action) => {
+      state.status = 'Fail';
+    });
+  },
+});
+
+export default userSlice.reducer;
