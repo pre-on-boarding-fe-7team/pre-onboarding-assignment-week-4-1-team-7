020bbf75b39cc6d74c8976555a53c674ed904303
cssong <a1373052011@gmail.com>
2022-09-22 15:59:13 +0900

    Feat: 추가, accounts, users api 호출 함수

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,20 @@
-const baseUrl = process.env.REACT_APP_SERVER_URL;
+import axios from 'axios';
 
-const get = async endpoint => {
-  const url = baseUrl + endpoint;
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw new Error(`${res.status.toString()} Error 인한 요청 실패!`);
-  }
-  const result = await res.json();
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAZ2FtaWwuY29tIiwiaWF0IjoxNjYzODI3NDA5LCJleHAiOjE2NjM4MzEwMDksInN1YiI6IjEwMSJ9.BVub25pvocTDUNlNS28Bl1LbvkeeiK7E97T1d_KEiXE';
 
-  return result;
+const api = axios.create({
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+});
+
+export const getAccountsApi = async params => {
+  const response = await api.get(`/accounts`, { params });
+  return { data: response.data, total: response.headers['x-total-count'] };
 };
 
-export { get };
+export const getUsersApi = async () => {
+  const response = await api.get(`/users`);
+  return response.data;
+};
