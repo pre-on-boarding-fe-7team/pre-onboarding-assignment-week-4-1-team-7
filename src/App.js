87329e5670ee04cb24a452f8c4ceca3244190c10
cssong <a1373052011@gmail.com>
2022-09-22 15:54:00 +0900

    Feat: 추가, accounts route

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import { ROUTE } from './common/utils/constant';
 import Main from './pages/Main/Main';
 import NotFound from './pages/NotFound/NotFound';
+import Accounts from './pages/Accounts/Accounts';
 
 function App() {
   return (
     <Routes>
       <Route path={ROUTE.MAIN} element={<Main />} />
+      <Route path={ROUTE.ACCOUNTS} element={<Accounts />} />
       <Route path={ROUTE.NOT_FOUND} element={<NotFound />} />
     </Routes>
   );
--- src/common/utils/constant.js
+++ src/common/utils/constant.js
@@ -1,4 +1,7 @@
 export const ROUTE = {
   MAIN: '/',
+  ACCOUNTS: '/accounts',
   NOT_FOUND: '*',
 };
+
+export const PAGE_LIMIT = 20;
