45ad269e5f92a31ea89f660f60532f40e31768ad
cssong <a1373052011@gmail.com>
2022-09-24 21:27:03 +0900

    Feat: 추가, 현재 페이지에 따른 title 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,22 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { getUsersThunk } from '../modules/usersSlice';
 import styled from '@emotion/styled';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
 import AppBar from '@mui/material/AppBar';
-import AccountCircle from '@mui/icons-material/AccountCircle';
 import Link from '@mui/material/Link';
 import { useLocation } from 'react-router-dom';
 
 const title = {
   accounts: '계좌 목록',
+  account: '계좌 정보',
   users: '사용자 목록',
+  user: '사용자 정보',
 };
 
-const Header = () => {
-  const dispatch = useDispatch();
-  const accounts = useSelector(state => state.users);
+const Header = ({ logout, isLogin }) => {
   const location = useLocation();
-  const pathname = location.pathname.replace('/', '');
+  const pathname = location.pathname.replace('/', '').split('/')[0];
 
-  useEffect(() => {
-    dispatch(getUsersThunk());
-  }, [dispatch]);
-
-  console.info();
   return (
     <AppBars position="sticky">
       <Toolbar
@@ -37,16 +27,10 @@ const Header = () => {
         <Typography component="h1" variant="h5" color="inherit" noWrap sx={{ flexGrow: 1 }}>
           <div>{title[pathname]}</div>
         </Typography>
-        {accounts.data !== null ? (
-          <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            color="inherit"
-          >
-            <AccountCircle />
-          </IconButton>
+        {isLogin() ? (
+          <Typography onClick={logout} color="inherit" variant="body2" underline="hover">
+            Logout
+          </Typography>
         ) : (
           <Link href="/login" color="inherit" variant="body2" underline="hover">
             Login
